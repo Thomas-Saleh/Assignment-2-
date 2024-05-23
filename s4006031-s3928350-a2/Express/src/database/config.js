b4477444010878a537bf0ca44a01245935e506d7
Thomas Saleh <s4006031@student.rmit.edu.au>
2024-05-23 12:41:36 +1000

    datbase, controllers and routes

---
+++ s4006031-s3928350-a2/Express/src/database/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    HOST: "rmit.australiaeast.cloudapp.azure.com",
+    USER: "s4006031_fsd_a2",
+    PASSWORD: "abc123",
+    DB: "s4006031_fsd_a2",
+    DIALECT: "mysql"
+  };
+  

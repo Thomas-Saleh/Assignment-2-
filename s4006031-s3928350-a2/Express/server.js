aa582a62f43b5ac3574e78bbba9cfdd2cc684941
Thomas Saleh <s4006031@student.rmit.edu.au>
2024-05-23 23:27:00 +1000

    more debugging

--- s4006031-s3928350-a2/Express/server.js
+++ s4006031-s3928350-a2/Express/server.js
@@ -16,12 +16,12 @@ app.get('/', (req, res) => {
 });
 
 // Import and use user routes
-require('./src/routes/user.routes')(express, app);
+require('./src/routes/users.routes')(express, app);
 require('./src/routes/isLoggedIn.routes')(express, app);
-require('./src/routes/product.routes')(express, app);
-require('./src/routes/review.routes')(express, app);
+require('./src/routes/products.routes')(express, app);
+require('./src/routes/reviews.routes')(express, app);
 require('./src/routes/cart.routes')(express, app);
-require('./src/routes/follow.routes')(express, app);
+require('./src/routes/follows.routes')(express, app);
 
 // Database synchronization and server start
 db.sequelize.sync().then(() => {

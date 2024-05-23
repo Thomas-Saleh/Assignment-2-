6a96a70260d28a07e75d8a7e17b45cb6a85fccfc
Thomas Saleh <s4006031@student.rmit.edu.au>
2024-05-23 23:03:48 +1000

    testing for erros

--- s4006031-s3928350-a2/Express/src/database/models/products.js
+++ s4006031-s3928350-a2/Express/src/database/models/products.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) =>
         primaryKey: true
       },
       name: {
-        type: DataTypes.STRING(100),
+        type: DataTypes.STRING(200),
         allowNull: false
       },
       description: {
